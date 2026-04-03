16582c5513a4ce9dbf494a57b847ff5f9980eff1
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-04 03:12:51 +0400

    Delete unnecessary code

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,33 +2,3 @@ import path from "path";
 import yaml from "yamljs";
 
 export const swaggerSpec = yaml.load(path.join(__dirname, "../../swagger.yml"));
-// import swaggerJsdoc from "swagger-jsdoc";
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "Todo API",
-//       version: "1.0.0",
-//       description: "Simple Todo REST API",
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:3008",
-//       },
-//     ],
-//     components: {
-//       securitySchemes: {
-//         ApiKeyAuth: {
-//           type: "apiKey",
-//           in: "header",
-//           name: "secret-api-key",
-//         },
-//       },
-//     },
-//     security: [{ ApiKeyAuth: [] }],
-//   },
-//   apis: ["./src/routes/*.ts"],
-// };
-
-// export const swaggerSpec = swaggerJsdoc(options);
