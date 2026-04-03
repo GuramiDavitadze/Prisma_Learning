286c128b07338c4e44d59c71d62e6ba804d55631
GuramiDavitadze <guramidavitadze2002@gmail.com>
2026-04-04 02:49:18 +0400

    Setup of Swagger is done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { type Application } from "express";
-import cors from 'cors'
+import cors from "cors";
 import helmet from "helmet";
 import { todosRoutes } from "./routes/todosRoutes.js";
 import { checkSecretKey } from "./middlewares/todosMiddlewares.js";
-const app:Application = express()
+import { swaggerSpec } from "./config/swagger.js";
+import swaggerUi from "swagger-ui-express";
+const app: Application = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(helmet())
-app.use(checkSecretKey)
-app.use('/api',todosRoutes)
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use("/api", checkSecretKey);
+app.use("/api", todosRoutes);
+app.use("/api-todos", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-export default app
+export default app;
--- src/routes/todosRoutes.ts
+++ src/routes/todosRoutes.ts
@@ -1,12 +1,18 @@
-import express from 'express'
-import { createNewTodo, deleteTodo, getAllData, getSingleTodo, updateTodo } from '../controllers/todosController.js'
-import { checkDataExist } from '../middlewares/todosMiddlewares.js'
+import express from "express";
+import {
+  createNewTodo,
+  deleteTodo,
+  getAllData,
+  getSingleTodo,
+  updateTodo,
+} from "../controllers/todosController.js";
+import { checkDataExist } from "../middlewares/todosMiddlewares.js";
 
-const todosRoutes = express.Router()
+const todosRoutes = express.Router();
 
-todosRoutes.get("/",getAllData)
-todosRoutes.get("/:id",getSingleTodo)
-todosRoutes.post('/',checkDataExist ,createNewTodo)
-todosRoutes.patch("/:id",updateTodo)
-todosRoutes.delete("/:id",deleteTodo)
-export {todosRoutes}
+todosRoutes.get("/", getAllData);
+todosRoutes.get("/:id", getSingleTodo);
+todosRoutes.post("/", checkDataExist, createNewTodo);
+todosRoutes.patch("/:id", updateTodo);
+todosRoutes.delete("/:id", deleteTodo);
+export { todosRoutes };
